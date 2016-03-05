93c0efea2dd56beac7e23947dc5d0afd2b362434
Jonathan Widro <jwidro@gmail.com>
2016-03-05 03:05:16 -0500

    new images checkin march 2016

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
-/* db connection  */
-$dbname = "db54729_1upguide";
-$connection = mysql_connect("internal-db.s54729.gridserver.com", "db54729", "O47jhg(%vfoyh") or die("Couldn't connect.");
+// Report all errors except E_NOTICE
+error_reporting(E_ALL & ~E_NOTICE);
+
+$thisurl =  $_SERVER['HTTP_HOST'];
+
+if(($thisurl=="www.wjdw.com")||($thisurl=="wjdw.com")){
+	$dbname = "db54729_1upguide";
+	$connection = mysql_connect("internal-db.s54729.gridserver.com", "db54729", "O47jhg(%vfoyh") or die("Couldn't connect.");
+}
+else{
+	$dbname = "1upguide";
+	$connection = mysql_connect("localhost", "root", "") or die("Couldn't connect.");
+}
 $db = mysql_select_db($dbname) or die("Couldn't select database");
 ?><html>
 <head>
