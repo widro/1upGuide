08e6bb6498463d4e66ff346682c1885341982f4d
Jonathan Widro <jwidro@gmail.com>
2019-06-22 21:26:41 -0700

    owb
    owb

---
+++ olivewoodbistro/menu.php
@@ -0,0 +1,126 @@
+<html>
+<head>
+<title>Olive Wood Bistro - the OWB in Los Angeles!</title>
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+</head>
+<body>
+
+
+<div class="container">
+	<div class="row" style="text-align:center;">
+		<img src="olivewoodbistro_logo20190517.jpg">
+	</div>
+	<div class="row" style="text-align:center;">
+		<h1>Summer 2019 Menu!!</h1>
+		<p>Check out all the great dishes</p>
+	</div>
+	<div class="row">
+		<div class="col-lg-6">
+			<h2>Fixin's Bar</h2>
+
+			<h5>House Pickles</h5>
+
+			<h5>Carmelized Onions</h5>
+
+			<h5>Jam of the Week</h5>
+
+
+
+			<h2>Small Plates</h2>
+			<img src="porkwontons.JPG" class="img-fluid">
+			<h5>Pork Wontons</h5>
+			<p style="font-style: italic;">Pork & scallion mixture, fried to perfection with our homemade soy ginger dipping sauce.</p>
+
+
+
+			<h2>Soups</h2>
+			<img src="frenchonionsoup.JPG" class="img-fluid">
+			<h5>French Onion Soup</h5>
+			<p style="font-style: italic;">Deep, rich beef broth, loaded with carmelized onions, served with four butter baguette gruyere crostini.</p>
+
+
+
+			<h2>Pasta</h2>
+			<img src="harissameatballs.JPG" class="img-fluid">
+			<h5>Harissa Meatballs</h5>
+			<p style="font-style: italic;">Slow simmered beef meatballs in OWB marinara, served over orzo.</p>
+
+			<img src="vegstiryfry.JPG" class="img-fluid">
+			<h5>Vegetable Stir-Fry</h5>
+			<p style="font-style: italic;">Mushrooms, Red bell peppers, carrots and baby corn stir fried in a soy/oyster sauce served over pasta.</p>
+
+
+			<img src="lasagna.JPG" class="img-fluid">
+			<h5>Lasagna</h5>
+			<p style="font-style: italic;">Flat pasta layered with garlic ricotta, pork sausage and OWB marinara</p>
+
+			<h2>From the Bakery</h2>
+
+			<img src="biscuits.JPG" class="img-fluid">
+			<h5>Buttermilk Biscuits</h5>
+			<p style="font-style: italic;">Flaky, buttery biscuits</p>
+
+
+			<img src="pretzels.JPG" class="img-fluid">
+			<h5>Pretzels</h5>
+			<p style="font-style: italic;">Baked from scratch to a golden brown, with a chewy inside, loaded with salt.</p>
+
+			<img src="ccc.JPG" class="img-fluid">
+			<h5>Chocolate Chip Cookies</h5>
+			<p style="font-style: italic;">World famous OWB specialty - large and soft, loaded with chocolate.</p>
+
+
+			<img src="cinnamonrolls.JPG" class="img-fluid">
+			<h5>Cinnamon Rolls</h5>
+			<p style="font-style: italic;">Baked daily in the OWB bakery, gooey buns with two types of icing - royal icing that melts into the layers as well as a cream cheese icing on top.</p>
+
+
+
+		</div>
+		<div class="col-lg-6">
+
+
+			<h2>Chicken</h2>
+			<img src="peanutchicken.JPG" class="img-fluid">
+			<h5>Peanut Chicken</h5>
+			<p style="font-style: italic;">Stir-fried white meat chicken with peanuts, snow peas and scallions. Served over garlic rice.</p>
+
+
+			<h2>Meat</h2>
+			<img src="beefstroganoff.JPG" class="img-fluid">
+			<h5>Beef Stroganoff</h5>
+			<p style="font-style: italic;">Sirloin cubes that melt in your mouth, smothered in creamy mushroom gravy and mixed with smooth egg noodles..</p>
+
+
+			<h2>From the Sea</h2>
+			<img src="searedsalmon.JPG" class="img-fluid">
+			<h5>Seared Salmon</h5>
+			<p style="font-style: italic;">Wild Alaskan salmon seared in a zesty spice blend, served with niscoise salad</p>
+
+			<img src="searedpollock.JPG" class="img-fluid">
+			<h5>Seared Pollock</h5>
+			<p style="font-style: italic;">Wild Alaskan Pollock seared in a zesty spice blend, served over wild rice & broccoli. </p>
+
+			<img src="shrimpandgrits.JPG" class="img-fluid">
+			<h5>Shrimp n Grits</h5>
+			<p style="font-style: italic;">A southern classic, OWB style! Shrimp in creole sauce, served with red bell peppers over cheddar grits.</p>
+
+
+
+		</div>
+	</div>
+</div>
+
+
+
+
+
+</body>
+</html>
